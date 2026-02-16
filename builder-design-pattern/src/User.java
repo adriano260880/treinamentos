e16eaab1b3803c8910de36ccfc981e58635cea3b
Adriano Lopes <adriano260880@gmail.com>
2026-02-16 12:49:25 -0300

    Builder design pattern

---
+++ builder-design-pattern/src/User.java
@@ -0,0 +1,85 @@
+
+public class User {
+
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", address='" + address + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public User(Builder builder) {
+        this.name = builder.name;
+        this.email = builder.email;
+        this.phone = builder.phone;
+        this.address = builder.address;
+        this.age = builder.age;
+    }
+
+    public static class Builder {
+        private final String name;
+        private String email;
+        private String phone;
+        private String address;
+        private int age;
+
+        public Builder(String name) {
+            this.name = name;
+        }
+
+        public Builder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder phone(String phone) {
+            this.phone = phone;
+            return this;
+        }
+
+        public Builder address(String address) {
+            this.address = address;
+            return this;
+        }
+
+        public Builder age(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public User build() {
+            return new User(this);
+        }
+    }
+}
+
